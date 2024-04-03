be61187a0a071b8359471548acb23bbdc91c76b7
gabrielCaio <email.caiogabriel@gmail.com>
2024-04-03 03:48:01 -0300

    feat: add cors support

--- rest-server/index.js
+++ rest-server/index.js
@@ -8,10 +8,26 @@ const port = 3000
 
 const app = express()
 
+// Add middlewares
 app.use(express.json())
 app.use(morgan('dev'))
+
+// Allow CORS
+app.use((req, res, next) => {
+
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
+
+  app.use(cors())
+
+  next()
+})
+
+// Initialize routes
 app.use("/", router)
 
+// Start server
 app.listen(port, () => {
   console.log(`${app_name} listening on port ${port}...`)
 })
